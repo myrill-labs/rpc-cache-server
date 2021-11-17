d769a1e8bf8e686c7be9db37e25d0c5ebaeb5578
nicolas carrara <nicolas.carrara1u@gmail.com>
2021-11-17 15:37:32 +0100

    k

--- rpc-cache-utils/src/config.ts
+++ rpc-cache-utils/src/config.ts
@@ -1,26 +1,26 @@
 export const settings = {
-  commitment: "recent",
-  cacheFunctions: {
-    names: ["getProgramAccounts"],
-    params: {
-      getProgramAccounts: [
-          "EidNXXqQS3xf51utL4UFWoyEE2ZUFcdL683cZnpBGqjJ",
-        // "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-        // "vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn",
-        //   "auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8",
-        //   "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98"
-      ],
+    commitment: "recent",
+    cacheFunctions: {
+        names: ["getProgramAccounts"],
+        params: {
+            getProgramAccounts: [
+                // "EidNXXqQS3xf51utL4UFWoyEE2ZUFcdL683cZnpBGqjJ",
+                // "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+                "vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn",
+                "auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8",
+                "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98"
+            ],
+        },
+        filters: {
+            getProgramAccounts: {
+                // "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98": [
+                //   [
+                //     {
+                //       dataSize: 388,
+                //     },
+                //   ],
+                // ],
+            },
+        },
     },
-    filters: {
-      getProgramAccounts: {
-        // "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98": [
-        //   [
-        //     {
-        //       dataSize: 388,
-        //     },
-        //   ],
-        // ],
-      },
-    },
-  },
 };
--- test.ts
+++ test.ts
@@ -85,10 +85,10 @@ const connection = new Connection(
     }
 );
 
-const programID = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+// const programID = "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 // const programID ="vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn"
 // const programID = "auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8"
-// const programID = "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98"
+const programID = "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98"
 // const programID = "EidNXXqQS3xf51utL4UFWoyEE2ZUFcdL683cZnpBGqjJ"
 
 connection.getProgramAccounts(
